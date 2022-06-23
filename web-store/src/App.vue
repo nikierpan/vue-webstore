a8849876aebb0c676bd38e0b76e4af84bbff6f58
nikierpan <napreikin@gmail.com>
2022-06-24 02:41:27 +0400

    Inializing vue app structure

---
+++ web-store/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <NavBar :AppName="this.appName"></NavBar>
+  <router-view/>
+</template>
+
+
+<style>
+
+</style>
+<script>
+import NavBar from "@/components/NavBar";
+
+export default {
+  name: 'App',
+  data () {
+      return {
+    appName: 'Vue Shop'
+    }
+  },
+  components: {NavBar}
+}
+</script>
---
+++ web-store/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">
+        {{ AppName }}
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: [
+    'AppName'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
